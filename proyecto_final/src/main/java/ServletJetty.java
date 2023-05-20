a16bdc32dbb236970bd099351d80a07a0d4adcf5
INGSYSTEM <juanromerocollazos@gmail.com>
2023-05-20 18:23:52 -0500

    configuración servlet

--- proyecto_final/src/main/java/ServletJetty.java
+++ proyecto_final/src/main/java/ServletJetty.java
@@ -4,10 +4,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
 @WebServlet(urlPatterns = "/servlet-jetty")
 public class ServletJetty extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    protected void doGet(HttpServletRequest request, @org.jetbrains.annotations.NotNull HttpServletResponse response) throws IOException {
         response.getWriter().println("¡Hola desde ServletJetty!");
     }
 }
