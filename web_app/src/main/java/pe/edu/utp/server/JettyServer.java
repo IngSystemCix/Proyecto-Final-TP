2bd8ce13fb87ef7f5fc252e9ef8dbb2fbcc74cda
INGSYSTEM <juanromerocollazos@gmail.com>
2023-05-28 18:32:13 -0500

    configure el servlet

---
+++ web_app/src/main/java/pe/edu/utp/server/JettyServer.java
@@ -0,0 +1,34 @@
+package pe.edu.utp.server;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerCollection;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+public class JettyServer {
+    public static void main(String[] args) throws Exception {
+        int port = 8080; // Puerto en el que se ejecutará el servidor
+
+        Server server = new Server(port);
+
+        // Configuración del contexto de la aplicación web
+        WebAppContext webAppContext = new WebAppContext();
+        webAppContext.setContextPath("/");
+        webAppContext.setWar("src/main/webapp"); // Ruta al directorio webapp de tu proyecto
+
+        // Configuración del handler del servidor
+        HandlerCollection handlers = new HandlerCollection();
+        handlers.addHandler(webAppContext);
+
+        server.setHandler(handlers);
+
+        server.start();
+        server.join();
+    }
+}
+
+
+
+
+
+
+
