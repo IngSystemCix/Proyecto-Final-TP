64dc756f882593105ebe1000f6b19c88214f041c
INGSYSTEM <juanromerocollazos@gmail.com>
2023-06-29 12:42:14 -0500

    Modificaciones en el reporte js css y html

--- proyecto_final/src/main/resources/report/js/main.js
+++ proyecto_final/src/main/resources/report/js/main.js
@@ -1,3 +1,93 @@
+const getOptionChart1 = () => {
+  return {
+    legend: {
+      top: 'bottom'
+    },
+    toolbox: {
+      show: true,
+      feature: {
+        mark: { show: true },
+        dataView: { show: true, readOnly: false },
+        restore: { show: true },
+        saveAsImage: { show: true }
+      }
+    },
+    series: [
+      {
+        name: 'Nightingale Chart',
+        type: 'pie',
+        radius: [50, 250],
+        center: ['50%', '50%'],
+        roseType: 'area',
+        itemStyle: {
+          borderRadius: 8
+        },
+        data: [
+          { value: 40, name: 'rose 1' },
+          { value: 38, name: 'rose 2' },
+          { value: 32, name: 'rose 3' },
+          { value: 30, name: 'rose 4' },
+          { value: 28, name: 'rose 5' },
+          { value: 26, name: 'rose 6' },
+          { value: 22, name: 'rose 7' },
+          { value: 18, name: 'rose 8' }
+        ]
+      }
+    ]
+  };
+};
+const getOptionChart2 = () => {
+  return {
+    tooltip: {
+      trigger: 'item'
+    },
+    legend: {
+      top: '5%',
+      left: 'center'
+    },
+    series: [
+      {
+        name: 'Access From',
+        type: 'pie',
+        radius: ['40%', '70%'],
+        avoidLabelOverlap: false,
+        itemStyle: {
+          borderRadius: 10,
+          borderColor: '#fff',
+          borderWidth: 2
+        },
+        label: {
+          show: false,
+          position: 'center'
+        },
+        emphasis: {
+          label: {
+            show: true,
+            fontSize: 40,
+            fontWeight: 'bold'
+          }
+        },
+        labelLine: {
+          show: false
+        },
+        data: [
+          { value: 1048, name: 'Search Engine' },
+          { value: 735, name: 'Direct' },
+          { value: 580, name: 'Email' },
+          { value: 484, name: 'Union Ads' },
+          { value: 300, name: 'Video Ads' }
+        ]
+      }
+    ]
+  };
+};
+const initCharts = () => {
+  const chart1 = echarts.init(document.getElementById("chart1"));
+  const chart2 = echarts.init(document.getElementById("chart2"));
+  chart1.setOption(getOptionChart1());
+  chart2.setOption(getOptionChart2());
+};
+
 window.addEventListener('load', () => {
-  console.log("ok");
+  initCharts();
 });
