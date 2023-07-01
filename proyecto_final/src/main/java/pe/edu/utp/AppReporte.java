abae45e089756ca05d91f8bf2a7a69853811ab2e
INGSYSTEM <juanromerocollazos@gmail.com>
2023-07-01 18:39:57 -0500

    Errores corregidos

--- proyecto_final/src/main/java/pe/edu/utp/AppReporte.java
+++ proyecto_final/src/main/java/pe/edu/utp/AppReporte.java
@@ -1,13 +1,12 @@
 package pe.edu.utp;
-
 import java.io.IOException;
 
 public class AppReporte {
     public static void main(String[] args) throws IOException{
         String fileName = "./src/main/resources/data.csv";
         ValidationData [] lista = InputData.loadData(fileName);
-        for (ValidationData data : lista) {
-            System.out.println(data);
+        for (ValidationData validate : lista) {
+            System.out.println(validate);
         }
     }
 }
