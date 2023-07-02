e6bffedcd1db25be2360e6b37ed082a5201708a1
INGSYSTEM <juanromerocollazos@gmail.com>
2023-07-01 21:25:06 -0500

    Actualización en el HTML5 y gráficas

--- proyecto_final/src/main/resources/report/js/main.js
+++ proyecto_final/src/main/resources/report/js/main.js
@@ -1,4 +1,4 @@
-// (Creador: Juan Bladimir Romero Collazos)
+// Gráficos (Creador: Juan Bladimir Romero Collazos)
 const getOptionChart1 = () => {
   return {
     xAxis: {
@@ -17,6 +17,26 @@ const getOptionChart1 = () => {
   };
 };
 const getOptionChart2 = () => {
+  return  {
+    xAxis: {
+      type: 'category',
+      boundaryGap: false,
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+      type: 'value'
+    },
+    series: [
+      {
+        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        type: 'line',
+        areaStyle: {}
+      }
+    ]
+  };
+};
+
+const getOptionChart3 = () => {
   return {
     tooltip: {
       trigger: 'item'
@@ -60,14 +80,84 @@ const getOptionChart2 = () => {
       }
     ]
   };
-};
+}
+
+const getOptionChart4 = () => {
+  return {
+    series: [
+      {
+        type: 'gauge',
+        progress: {
+          show: true,
+          width: 18
+        },
+        axisLine: {
+          lineStyle: {
+            width: 18
+          }
+        },
+        axisTick: {
+          show: false
+        },
+        splitLine: {
+          length: 15,
+          lineStyle: {
+            width: 2,
+            color: '#999'
+          }
+        },
+        axisLabel: {
+          distance: 25,
+          color: '#999',
+          fontSize: 20
+        },
+        anchor: {
+          show: true,
+          showAbove: true,
+          size: 25,
+          itemStyle: {
+            borderWidth: 10
+          }
+        },
+        title: {
+          show: false
+        },
+        detail: {
+          valueAnimation: true,
+          fontSize: 80,
+          offsetCenter: [0, '70%']
+        },
+        data: [
+          {
+            value: 50
+          }
+        ]
+      }
+    ]
+  };
+}
+
 const initCharts = () => {
   const chart1 = echarts.init(document.getElementById("chart1"));
   const chart2 = echarts.init(document.getElementById("chart2"));
+  const chart3 = echarts.init(document.getElementById("chart3"));
+  const chart4 = echarts.init(document.getElementById("chart4"));
   chart1.setOption(getOptionChart1());
   chart2.setOption(getOptionChart2());
+  chart3.setOption(getOptionChart3());
+  chart4.setOption(getOptionChart4());
 };
 
 window.addEventListener('load', () => {
   initCharts();
 });
+
+
+// procentajes (Creador: Juan Bladimir Romero Collazos)
+
+const totalDatos = 22712;
+const numeroCoincidencias = 2;
+const porcentaje = (100 * numeroCoincidencias) / totalDatos;
+const porcentajeCelda = document.getElementById("porcentaje");
+
+porcentajeCelda.textContent = `${porcentaje.toFixed(2)}%`
