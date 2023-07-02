45d6510f21b555c4ecb0c9b916e0f0329744a592
INGSYSTEM <juanromerocollazos@gmail.com>
2023-07-01 19:45:52 -0500

    Actualización en el HTML5

--- proyecto_final/src/main/resources/report/js/main.js
+++ proyecto_final/src/main/resources/report/js/main.js
@@ -1,37 +1,17 @@
+// (Creador: Juan Bladimir Romero Collazos)
 const getOptionChart1 = () => {
   return {
-    legend: {
-      top: 'bottom'
+    xAxis: {
+      type: 'category',
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
     },
-    toolbox: {
-      show: true,
-      feature: {
-        mark: { show: true },
-        dataView: { show: true, readOnly: false },
-        restore: { show: true },
-        saveAsImage: { show: true }
-      }
+    yAxis: {
+      type: 'value'
     },
     series: [
       {
-        name: 'Nightingale Chart',
-        type: 'pie',
-        radius: [50, 250],
-        center: ['50%', '50%'],
-        roseType: 'area',
-        itemStyle: {
-          borderRadius: 8
-        },
-        data: [
-          { value: 40, name: 'rose 1' },
-          { value: 38, name: 'rose 2' },
-          { value: 32, name: 'rose 3' },
-          { value: 30, name: 'rose 4' },
-          { value: 28, name: 'rose 5' },
-          { value: 26, name: 'rose 6' },
-          { value: 22, name: 'rose 7' },
-          { value: 18, name: 'rose 8' }
-        ]
+        data: [120, 200, 150, 80, 70, 110, 130],
+        type: 'bar'
       }
     ]
   };
