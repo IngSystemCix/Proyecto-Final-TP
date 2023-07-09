9349769d4ed09e1d2b6d8838c2daa5216b024a7f
INGSYSTEM <juanromerocollazos@gmail.com>
2023-07-08 20:37:54 -0500

    He actualizado la documentacion del programa

--- proyecto_final/src/main/java/pe/edu/utp/util/InputDataFormattedDateTime.java
+++ proyecto_final/src/main/java/pe/edu/utp/util/InputDataFormattedDateTime.java
@@ -8,7 +8,7 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 /**
- * Este código permite darle formato a las fechas
+ * Este código permite darle formato a las fechas y a las horas del archivo CSV
  * @author Juan Bladimir Romero Collazos
  * @version 0.0.1V
  */
