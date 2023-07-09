e5998955f077cba7fbdfdd1c4b03e0efd099e7a3
INGSYSTEM <juanromerocollazos@gmail.com>
2023-07-08 20:44:19 -0500

    Actualice el nombre la función

--- proyecto_final/src/main/java/pe/edu/utp/AppReportDataShow.java
+++ proyecto_final/src/main/java/pe/edu/utp/AppReportDataShow.java
@@ -13,7 +13,7 @@ public class AppReportDataShow {
      * Nos permite vizualizar cada dato de forma iterativa
      * @throws IOException  Significa que el método puede generar una excepción de entrada y salida.
      */
-    public static void mostrarDatos() throws IOException{
+    public static void showData() throws IOException{
         // Este código nos permite visualizar los datos (Autor: Juan Bladimir Romero Collazos)
         String fileName = "./src/main/resources/data.csv";
         ValidationData [] lista = InputDataFormattedDateTime.dateValidateData(fileName, 2000);
