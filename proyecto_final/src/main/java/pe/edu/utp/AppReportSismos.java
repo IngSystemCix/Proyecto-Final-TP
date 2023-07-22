f666031179a48419d6fa17a33c968a25c660e641
INGSYSTEM <juanromerocollazos@gmail.com>
2023-07-22 01:15:42 -0500

    Actualización del programa

--- proyecto_final/src/main/java/pe/edu/utp/AppReportSismos.java
+++ proyecto_final/src/main/java/pe/edu/utp/AppReportSismos.java
@@ -10,6 +10,12 @@ import java.util.Scanner;
 public class AppReportSismos {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        String preguntaASCIIOrHTML5 = """
+                ¿Cómo desea imprimir su reporte (ASCCI o HTML5)?
+                0) ASCII
+                1) HTML5
+                Selecciona una opción:
+                """;
         String menu = """
                 ################################################################################################
                 # A. Tabla con el número de eventos sísmicos por año dado un rango de años.                    #
@@ -29,34 +35,20 @@ public class AppReportSismos {
             String opc = input.nextLine().toUpperCase();
             switch (opc) {
                 case "A" -> {
-                    // Configuración de tu la opción "A" (Autor: )
-                    // Función para validar que opción de reporte (Autor: Juan Bladimir Romero Collazos)
-                    String resultado = reportASCIIorHTML5();
-                    System.out.println(resultado);
+                    // Configuración de tu la opción "A" (Autor: );
                 }
                 case "B" -> {
                     // Configuración de tu la opción "B" (Autor: )
                     // Función para validar que opción de reporte (Autor: Juan Bladimir Romero Collazos)
-                    String resultado = reportASCIIorHTML5();
-                    System.out.println(resultado);
                 }
                 case "C" -> {
                     // Configuración de tu la opción "C" (Autor: )
-                    // Función para validar que opción de reporte (Autor: Juan Bladimir Romero Collazos)
-                    String resultado = reportASCIIorHTML5();
-                    System.out.println(resultado);
                 }
                 case "D" -> {
                     // Configuración de tu la opción "D" (Autor: )
-                    // Función para validar que opción de reporte (Autor: Juan Bladimir Romero Collazos)
-                    String resultado = reportASCIIorHTML5();
-                    System.out.println(resultado);
                 }
                 case "E" -> {
                     // Configuración de tu la opción "E" (Autor: )
-                    // Función para validar que opción de reporte (Autor: Juan Bladimir Romero Collazos)
-                    String resultado = reportASCIIorHTML5();
-                    System.out.println(resultado);
                 }
                 case "F" -> {
                     System.out.println("Gracias por usar el programa");
@@ -67,24 +59,5 @@ public class AppReportSismos {
                 }
             }
         }while (continuar);
-    }
-    /**
-     * Creacion de la función para poder hacer la pregunta de que tipo de reporte desea el usuario.
-     * @return Si la opción es 0 o 1 para mandar un mensaje por consola.
-     */
-    /* (Autor: Hugo Fupuy Chanamé)        */
-    public static String reportASCIIorHTML5 () {
-        Scanner input = new Scanner(System.in);
-        String pregunta = """
-                ¿Cómo desea imprimir su reporte (ASCCI o HTML5)?
-                0) ASCII
-                1) HTML5
-                Selecciona una opción:
-                """;
-        System.out.println(pregunta);
-        int opc = input.nextInt();input.nextLine();
-        return (opc == 0) ? "Espere porfavor se esta creado el reporte ASCII..." :
-                (opc == 1) ? "Espere porfavor se esta creado el reporte HTML..." :
-                        "Usted a ingresado una opción invalida";
     }
 }
